0d984e88e55848a2f3c71fa19826c99826ecb908
pauliejog <Elakpjd7!>
2020-03-02 14:44:53 -0700

    added some code that might work sortOfSort

--- java/SortOfSort.java
+++ java/SortOfSort.java
@@ -1,5 +1,11 @@
 public class SortOfSort {
     public void sortOfSort(int[] arr) {
+        int tempRight = arr.length - 1;
+        int tempLeft = 0;
+        int next = arr.length;
+        while(next > 0) {
+
+        }
 
     }//end sortOfSort
 
@@ -13,6 +19,18 @@ public class SortOfSort {
         }//end for
         return max;
     }//end findMax
+
+    /*some code that might work
+      public static int findMax(int[] arr, int left, int right) {
+    int max = 0;
+    //int mid = arr.length/2;
+    for(int i = left; i < right; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }//end for
+    return max;
+  }//end findMax
+     */
 }//end class
 
 
