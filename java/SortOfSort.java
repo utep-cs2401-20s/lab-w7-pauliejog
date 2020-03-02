c6116d166843df3a0432d9dd7742760ec64f1a78
pauliejog <Elakpjd7!>
2020-03-02 14:47:28 -0700

    added some code that might work sortOfSort

--- java/SortOfSort.java
+++ java/SortOfSort.java
@@ -4,17 +4,16 @@ public class SortOfSort {
         int tempLeft = 0;
         int next = arr.length;
         while(next > 0) {
+            arr[tempRight] = findMax(arr)
 
         }
 
     }//end sortOfSort
 
-    public int findMax(int[] arr, int a) {//will find next max given the previous max value, first pass a = 0
-        int max = arr[0];
-        if(max == a)
-            max = arr[1];
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] > max && arr[i] != a)
+    public int findMax(int[] arr, int left, int right) {
+        int max = 0;
+        for(int i = left; i <= right; i++) {
+            if(arr[i] > max)
                 max = arr[i];
         }//end for
         return max;
@@ -24,7 +23,7 @@ public class SortOfSort {
       public static int findMax(int[] arr, int left, int right) {
     int max = 0;
     //int mid = arr.length/2;
-    for(int i = left; i < right; i++) {
+    for(int i = left; i <= right; i++) {
         if(arr[i] > max)
             max = arr[i];
     }//end for
