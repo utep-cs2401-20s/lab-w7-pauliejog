855494693d41b16a5336870c272b848a1859511e
pauliejog <Elakpjd7!>
2020-03-02 17:15:11 -0700

    modified sortOfSort method

--- java/SortOfSort.java
+++ java/SortOfSort.java
@@ -2,22 +2,49 @@ public class SortOfSort {
     public void sortOfSort(int[] arr) {
         int tempRight = arr.length - 1;
         int tempLeft = 0;
-        int next = arr.length;
-        while(next > 0) {
-            arr[tempRight] = findMax(arr)
 
+        while(tempLeft < tempRight) {
+            int tempHoldR = arr[tempRight];
+            int tempHoldL = arr[tempLeft];
+
+            arr[tempRight] = findMax(arr,tempLeft,tempRight);
+            arr[findIndexOfMax(arr,tempLeft,tempRight)] = tempHoldR;
+            tempRight--;
+            tempHoldR = arr[tempRight];
+
+            arr[tempRight] = findMax(arr,tempLeft,tempRight);
+            arr[findIndexOfMax(arr,tempLeft,tempRight)] = tempHoldR;
+            tempRight--;
+            tempHoldR = arr[tempRight];
+
+            arr[tempLeft] = findMax(arr,tempLeft,tempRight);
+            tempLeft++;
+            arr[tempLeft] = findMax(arr,tempLeft,tempRight);
         }
 
     }//end sortOfSort
 
     public int findMax(int[] arr, int left, int right) {
         int max = 0;
-        for(int i = left; i <= right; i++) {
+        int i;
+        for(i = left; i <= right; i++) {
             if(arr[i] > max)
                 max = arr[i];
         }//end for
         return max;
     }//end findMax
+
+    public int findIndexOfMax(int[] arr, int left, int right) {
+        int max = 0;
+        int index = 0;
+        for(int i = left; i <= right; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+                index = i;
+            }
+        }//end for
+        return index;
+    }//end findIndexOfMax
 
     /*some code that might work
       public static int findMax(int[] arr, int left, int right) {
