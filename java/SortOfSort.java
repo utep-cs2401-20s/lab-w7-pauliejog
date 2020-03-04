1a7e7e353257cb1124b6274cd0e324e0639981d0
pauliejog <Elakpjd7!>
2020-03-04 15:35:26 -0700

    completed sortOfSort method

--- java/SortOfSort.java
+++ java/SortOfSort.java
@@ -2,45 +2,56 @@ public class SortOfSort {
     public void sortOfSort(int[] arr) {
         int tempRight = arr.length - 1;
         int tempLeft = 0;
+        int n, max, maxInd;
 
-        //int track = arr.length;
-        int n = arr.length/4;
-        int max, maxInd;
+        if(arr.length%4 == 0)
+            n = arr.length/4;
+        else
+            n = arr.length/4 + 1;
+
         while(n != 0) {
             int tempHoldR = arr[tempRight];
             int tempHoldL = arr[tempLeft];
 
+            if(tempLeft > tempRight)
+                break;
             max = findMax(arr, tempLeft, tempRight);
             maxInd = findIndexOfMax(arr, tempLeft, tempRight);
             arr[tempRight] = max;
             if(maxInd != tempRight)
                 arr[maxInd] = tempHoldR;
             tempRight--;
-            tempHoldR = arr[tempRight];
+            if(tempRight > -1)
+                tempHoldR = arr[tempRight];
 
+            if(tempLeft > tempRight)
+                break;
             max = findMax(arr, tempLeft, tempRight);
             maxInd = findIndexOfMax(arr, tempLeft, tempRight);
             arr[tempRight] = max;
             if(maxInd != tempRight)
                 arr[maxInd] = tempHoldR;
             tempRight--;
-            //tempHoldR = arr[tempRight];
 
+            if(tempLeft > tempRight)
+                break;
             max = findMax(arr, tempLeft, tempRight);
             maxInd = findIndexOfMax(arr, tempLeft, tempRight);
             arr[tempLeft] = max;
             if(maxInd != tempLeft)
                 arr[maxInd] = tempHoldL;
             tempLeft++;
-            tempHoldL = arr[tempLeft];
+            if(tempLeft < arr.length)
+                tempHoldL = arr[tempLeft];
 
+            if(tempLeft > tempRight)
+                break;
             max = findMax(arr, tempLeft, tempRight);
             maxInd = findIndexOfMax(arr, tempLeft, tempRight);
             arr[tempLeft] = max;
             if(maxInd != tempLeft)
                 arr[maxInd] = tempHoldL;
             tempLeft++;
-            //tempHoldL = arr[tempLeft];
 
             n--;
         }
